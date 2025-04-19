a735be4f77cc67d0fc80feae1d8b77dc2cb7e00e
Cj-bc <cj.bc-sd@outlook.jp>
2025-04-19 21:55:25 +0900

    REMOVE ALL ASSETS TO START OVER

--- Packages/com.github.cj-bc.configurables/Runtime/Configurables/IConfigurable.cs
+++
@@ -1,9 +0,0 @@
-namespace Configurables
-{
-    /// <summary>A class that uses <c>Config</c> instance to configure itself.</summary>
-    public interface IConfigurable<Config>
-    {
-        /// <summary>Configures this class/struct with given config value.</summary>
-        public void Configure(Config config);
-    }
-}
