799de18b8852a67b92f7b9503f5b6a2a86e93992
Cj-bc <cj.bc-sd@outlook.jp>
2025-04-30 12:23:50 +0900

    refactor: Move ConfigProviderSelector to Runtime asmdef

--- src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Editor/ConfigProviderSelectorEditor.cs
+++ src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Editor/ConfigProviderSelectorEditor.cs
@@ -6,10 +6,6 @@ using UnityEditor.UIElements;
 /// https://docs.unity3d.com/ja/2022.3/ScriptReference/TypeCache.GetTypesDerivedFrom.html
 /// https://light11.hatenadiary.com/entry/2021/04/26/202054
 
-[Serializable]
-internal class ConfigProviderSelector
-{}
-
 [CustomPropertyDrawer(typeof(ConfigProviderSelector))]
 internal class ConfigProviderSelectorEditor : PropertyDrawer
 {
---
+++ src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Runtime/ConfigProviderSelector.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+internal class ConfigProviderSelector
+{
+    public IConfigProvider ConfigProvider;
+}
+
