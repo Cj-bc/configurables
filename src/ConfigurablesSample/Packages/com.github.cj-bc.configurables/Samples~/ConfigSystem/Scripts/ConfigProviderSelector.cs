c47e60436f11fc71daf362b582141b90a52f368c
Cj-bc <cj.bc-sd@outlook.jp>
2025-10-02 01:01:02 +0900

    fix: ConfigProviderSelector should have object for compability

--- src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Samples~/ConfigSystem/Scripts/ConfigProviderSelector.cs
+++ src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Samples~/ConfigSystem/Scripts/ConfigProviderSelector.cs
@@ -5,6 +5,18 @@ namespace Configurables
     [Serializable]
     public class ConfigProviderSelector
     {
-        public IConfigProvider ConfigProvider;
+        private object rawObject;
+        public IConfigProvider ConfigProvider
+        {
+            set {
+                if (value is IConfigProvider provider) rawObject = value;
+                OnValueChanged?.Invoke(value);
+            }
+            get => rawObject as IConfigProvider;
+        }
+
+        public event Action<IConfigProvider> OnValueChanged;
+
+
     }
 }
