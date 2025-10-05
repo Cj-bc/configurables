eedd9f516f00c4d2be87b6dc9e87bb5beed1c983
Cj-bc <cj.bc-sd@outlook.jp>
2025-10-05 11:36:06 +0900

    feat: Use SerializeReference to serialize IConfigProvider implementation

--- src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Editor/ConfigProviderSelectorEditor.cs
+++ src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Editor/ConfigProviderSelectorEditor.cs
@@ -16,21 +16,19 @@ internal class ConfigProviderSelectorEditor : PropertyDrawer
 {
     public override VisualElement CreatePropertyGUI(SerializedProperty property)
     {
-        var rawObjectState = property.FindPropertyRelative("__serializedRawObject");
-        var rawObjectType = property.FindPropertyRelative("__serializedRawObjectType");
+        var rawObject = property.FindPropertyRelative("rawObject");
         var container = new VisualElement();
 
         var foundProviders = AppDomain.CurrentDomain.GetAssemblies().SelectMany(asm => asm.GetTypes())
             .Where(t => t.IsClass && t.GetInterfaces().Select(i => i.Name).Contains(typeof(IConfigProvider).Name));
 
         Dictionary<string, Type> nameToType = foundProviders.ToDictionary(t => t.ToString());
+
         var selector = new DropdownField("IConfigProvider", foundProviders.Select(t => t.ToString()).Prepend("None").ToList(),
-                                         rawObjectType.stringValue != "null" ? "None" : "None", a => a, a => a);
+                                         rawObject.boxedValue is "null" ? "None" : rawObject.GetType().Name, a => a, a => a);
         selector.RegisterValueChangedCallback(ev =>
         {
-            Type t = nameToType[ev.newValue];
-            rawObjectState.stringValue = "";
-            rawObjectType.stringValue = t.AssemblyQualifiedName;
+            rawObject.boxedValue = nameToType.TryGetValue(ev.newValue, out Type t) ? Activator.CreateInstance(t) : null;
         });
 
         container.Add(selector);
--- src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Samples~/ConfigSystem/Scripts/ConfigProviderSelector.cs
+++ src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Samples~/ConfigSystem/Scripts/ConfigProviderSelector.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 namespace Configurables
 {
     [Serializable]
-    public class ConfigProviderSelector : ISerializationCallbackReceiver
+    public class ConfigProviderSelector
     {
-        private object rawObject;
-
-        [SerializeField] private string __serializedRawObject;
-        [SerializeField] private string __serializedRawObjectType;
+        [SerializeReference] private IConfigProvider rawObject;
 
         public IConfigProvider ConfigProvider
         {
@@ -24,28 +21,5 @@ namespace Configurables
         }
 
         public event Action<IConfigProvider> OnValueChanged;
-
-        public void OnBeforeSerialize()
-        {
-            if (rawObject is null)
-            {
-                __serializedRawObjectType = "null";
-                return;
-            }
-
-            __serializedRawObject = JsonUtility.ToJson(rawObject);
-            __serializedRawObjectType = rawObject.GetType().AssemblyQualifiedName;
-        }
-
-        public void OnAfterDeserialize()
-        {
-            if (__serializedRawObjectType == "null")
-            {
-                rawObject = null;
-                return;
-            }
-
-            rawObject = JsonUtility.FromJson(__serializedRawObject, Type.GetType(__serializedRawObjectType));
-        }
     }
 }
