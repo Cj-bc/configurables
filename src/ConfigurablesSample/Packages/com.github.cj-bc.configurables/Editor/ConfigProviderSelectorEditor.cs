8fcb53d17357a32baf6b70418cfc7373668a4ab9
Cj-bc <cj.bc-sd@outlook.jp>
2025-10-05 09:51:24 +0900

    feat: Serialize string representation of rawObject

--- src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Editor/ConfigProviderSelectorEditor.cs
+++ src/ConfigurablesSample/Packages/com.github.cj-bc.configurables/Editor/ConfigProviderSelectorEditor.cs
@@ -16,14 +16,23 @@ internal class ConfigProviderSelectorEditor : PropertyDrawer
 {
     public override VisualElement CreatePropertyGUI(SerializedProperty property)
     {
-        var rawObject = property.FindPropertyRelative("rawObject");
+        var rawObjectState = property.FindPropertyRelative("__serializedRawObject");
+        var rawObjectType = property.FindPropertyRelative("__serializedRawObjectType");
         var container = new VisualElement();
 
         var foundProviders = AppDomain.CurrentDomain.GetAssemblies().SelectMany(asm => asm.GetTypes())
             .Where(t => t.IsClass && t.GetInterfaces().Select(i => i.Name).Contains(typeof(IConfigProvider).Name));
 
+        Dictionary<string, Type> nameToType = foundProviders.ToDictionary(t => t.ToString());
         var selector = new DropdownField("IConfigProvider", foundProviders.Select(t => t.ToString()).Prepend("None").ToList(),
-                                         rawObject is IConfigProvider serializedProvider ? serializedProvider.GetType().Name : "None", a => a, a => a);
+                                         rawObjectType.stringValue != "null" ? "None" : "None", a => a, a => a);
+        selector.RegisterValueChangedCallback(ev =>
+        {
+            Type t = nameToType[ev.newValue];
+            rawObjectState.stringValue = "";
+            rawObjectType.stringValue = t.AssemblyQualifiedName;
+        });
+
         container.Add(selector);
         return container;
     }
